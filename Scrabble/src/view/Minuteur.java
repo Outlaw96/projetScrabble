721148ca74a17d57aa4e552c6acda8d6f804ff9d
nicolas <zafitinanicolas@yahoo.fr>
2017-10-20 11:14:20 +0200

    [dev] ajout minuteur [à débugger]

--- Scrabble/src/view/Minuteur.java
+++ Scrabble/src/view/Minuteur.java
@@ -1,31 +1,85 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
-public class Minuteur extends JPanel{
-
+public class Minuteur extends JPanel implements Runnable {
+	private JLabel affichage;
 	private String message;
-	
-	public Minuteur()
-	{
+	private int minutes;
+	private int secondes;
+	private boolean start;
+	private Thread t;
+	private Font font;
+
+	public Minuteur(boolean start, int minutes) {
 		/*---------------------------------------------------
 		 * Initialisation des paramètres du minuteur
 		 * --------------------------------------------------
 		 */
 		this.setBackground(new Color(84, 130, 53));
-		this.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK),"Minuteur",TitledBorder.CENTER, TitledBorder.TOP, new Font("Serial", Font.BOLD, 25)));
-		this.updateContent("00 : 00");
+		this.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), "Minuteur",
+				TitledBorder.CENTER, TitledBorder.TOP, new Font("Serial", Font.BOLD, 25)));
+		// initialisation du minuteur
+		this.start = start;
+		this.minutes = minutes;
+		this.secondes = 59;
+		this.font = new Font("Courier new", Font.BOLD, 30);
+		this.affichage = new JLabel(this.minutes + " " + this.secondes);
+		this.affichage.setFont(font);
+		this.add(affichage);
+		t = new Thread(this);
+		t.start();
 	}
-	
-	public void updateContent(String msg)
-	{
-		JLabel contenuMessage = new JLabel(msg);
-		contenuMessage.setFont(new Font("Serial", Font.BOLD, 50));
-		this.add(contenuMessage);
+
+	public void run() {
+		while (this.start == true) {
+			try {
+				Thread.sleep(1000);
+				this.secondes--;
+				if (this.secondes == -1) {
+					this.secondes = 59;
+					this.minutes--;
+				}
+				if (this.secondes == 0 && this.minutes == 0) {
+					this.start = false;
+				}
+				message = this.minutes + " : " + this.secondes;
+				this.affichage.setText(message);
+				System.out.println(message);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+	public int getSecondes() {
+		return secondes;
+	}
+
+	public void setSecondes(int secondes) {
+		this.secondes = secondes;
+	}
+
+	public boolean isStart() {
+		return start;
+	}
+
+	public void setStart(boolean start) {
+		this.start = start;
 	}
 }
--- Scrabble/src/view/ToolMenu.java
+++ Scrabble/src/view/ToolMenu.java
@@ -5,21 +5,20 @@ import java.awt.Color;
 
 import javax.swing.*;
 
-public class ToolMenu extends JPanel{
+public class ToolMenu extends JPanel {
 
 	private Minuteur minuteur;
 	private ScoreJoueurs scoreJoueurs;
 	private Dictionnaire dictionnaire;
-	
-	public ToolMenu()
-	{
+
+	public ToolMenu() {
 		this.setLayout(new BorderLayout());
 		this.setBackground(new Color(31, 78, 120));
-		
-		this.minuteur = new Minuteur();
-		this.scoreJoueurs= new ScoreJoueurs();
+
+		this.minuteur = new Minuteur(true, 1);
+		this.scoreJoueurs = new ScoreJoueurs();
 		this.dictionnaire = new Dictionnaire();
-		
+
 		this.add(minuteur, BorderLayout.NORTH);
 		this.add(scoreJoueurs, BorderLayout.CENTER);
 		this.add(dictionnaire, BorderLayout.SOUTH);
